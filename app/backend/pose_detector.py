aafdc2b897721001cab8b82b4521bcaabcf9117d
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2025-07-25 21:38:55 -0400

    feat(backend): Add functionality to get landmark position and angle between three landmark points

--- app/backend/pose_detector.py
+++ app/backend/pose_detector.py
@@ -45,4 +45,27 @@ class PoseDetector:
                 self.utilDraw.draw_landmarks(RGBImg, self.results.pose_landmarks, self.poseSolution.POSE_CONNECTIONS)
 
         return RGBImg
+    
+    def find_cords(self, img):
+        self.lmList=[]
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w= img.shape
+                cx, cy = int(lm.x*w),int(lm.y*h)
+                self.lmList.append([id, cx, cy])
+
+        return self.lmList
+
+    def find_angle(self, img, p1, p2, p3):
+        x1, y1= self.lmList[p1][1:]
+        x2, y2= self.lmList[p2][1:]
+        x3, y3= self.lmList[p3][1:]
+
+        angle=math.degrees(math.atan2(y3-y2, x3-x2)-math.atan2(y1-y2, x1-x2))
+
+        if angle<0:
+            angle+=360
+
+        return angle
+        
                 
