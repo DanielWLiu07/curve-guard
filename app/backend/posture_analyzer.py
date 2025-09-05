dda3c0dd41377e0b4b664f7626a1ced7e5da5a3f
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2025-09-04 23:49:47 -0400

    refactor(backend): remove redundant variables in eye and shoulder checker; reuse old variables for simplicity and clarity

--- app/backend/posture_analyzer.py
+++ app/backend/posture_analyzer.py
@@ -43,37 +43,33 @@ class PostureAnalyzer(QObject):
                     cv.line(processed_img, (0, int(self.eye_level)), (9999, int(self.eye_level)), (255, 0, 0), 2)
 
                 if self.shoulder_visibility:
-                    left_shoulder = (self.lmList[11][1], self.lmList[11][2])
-                    right_shoulder = (self.lmList[12][1], self.lmList[12][2])
+                    left_shoulder = self.lmList[11]
+                    right_shoulder = self.lmList[12]
                     
-                    cv.circle(processed_img, left_shoulder, 30, (255, 0, 0), 3)
-                    cv.circle(processed_img, right_shoulder, 30, (255, 0, 0), 3)
-                    cv.line(processed_img, left_shoulder, right_shoulder, (255, 0, 0), 2)
+                    cv.circle(processed_img, left_shoulder[1:], 30, (255, 0, 0), 3)
+                    cv.circle(processed_img, right_shoulder[1:], 30, (255, 0, 0), 3)
+                    cv.line(processed_img, left_shoulder[1:], right_shoulder[1:], (255, 0, 0), 2)
 
-                    y1 = self.lmList[11][2]
-                    y2 = self.lmList[12][2]
-                    vertical_dist = abs(y1 - y2)
+                    vertical_dist = abs(left_shoulder[2] - right_shoulder[2])
                     text = f"Vertical Dist: {vertical_dist}px"
 
                     x_pos = (self.lmList[11][1] + self.lmList[12][1]) // 2
-                    y_pos = min(y1, y2) - 20
+                    y_pos = min(left_shoulder[2], right_shoulder[2]) - 20
                     cv.putText(processed_img, text, (x_pos, y_pos), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
                 
                 if self.head_visibility:
                     left_eye = self.lmList[2]
                     right_eye = self.lmList[5]
 
-                    cv.circle(processed_img, left_eye, 30, (255, 0, 0), 3)
-                    cv.circle(processed_img, right_eye, 30, (255, 0, 0), 3)
-                    cv.line(processed_img, left_eye, right_eye, (255, 0, 0), 2)
+                    cv.circle(processed_img, left_eye[1:], 30, (255, 0, 0), 3)
+                    cv.circle(processed_img, right_eye[1:], 30, (255, 0, 0), 3)
+                    cv.line(processed_img, left_eye[1:], right_eye[1:], (255, 0, 0), 2)
 
-                    y1 = left_eye[2]
-                    y2 = right_eye[2]
-                    vertical_dist = abs(y1 - y2)
+                    vertical_dist = abs(left_shoulder[2] - right_shoulder[2])
                     text = f"Vertical Dist: {vertical_dist}px"
 
                     x_pos = (self.lmList[2][1] + self.lmList[5][1]) // 2
-                    y_pos = min(y1, y2) - 20
+                    y_pos = min(left_eye[2], right_eye[2]) - 20
                     cv.putText(processed_img, text, (x_pos, y_pos), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
                 # eye level detecion
                 self.check_eye_level()
