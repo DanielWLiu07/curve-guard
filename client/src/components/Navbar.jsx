363d104bb1798b5f45fe3f4171b0ddba740f71ae
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2025-11-01 11:15:54 -0400

    feat: update logo and remove navbar blur

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from "@radix-ui/themes";
 
 export default function Navbar({ signOut, user }) {
   return (
-    <header className="w-full bg-jade-12/70 backdrop-blur z-20 relative">
+    <header className="w-full bg-jade-12/70 transparent z-20 relative">
       <div className="mx-auto w-full max-w-[102rem] pl-12 pr-6 py-4 flex items-center justify-between">
         <div className="flex items-center gap-4">
           <img src="images/logo.png" alt="CurveGuard Logo" className="h-14 w-auto" />
@@ -21,12 +22,10 @@ export default function Navbar({ signOut, user }) {
               Sign out
             </button>
           ) : (
-            <Link
-              to="/signin"
-              className="inline-flex items-center rounded-md bg-blue-9 px-3 py-1.5 text-white text-sm font-medium shadow hover:bg-blue-10 focus:outline-none focus:ring-2 focus:ring-blue-7 focus:ring-offset-2 transition-colors"
-            >
-              Sign in
-            </Link>
+            <Button color="gray" variant="outline" highContrast>
+                Sign In
+              </Button>
+            
           )}
         </nav>
       </div>
